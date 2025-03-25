a78259d7fc80c179fe0a8e8585b8b4225fdd0dff
MARLOWE IAN JUMAGBAS <hed-mijumagbas@smu.edu.ph>
2025-03-25 17:48:15 +0800

    all

---
+++ PetNameGeneratorMIJ/PetNameGeneratorMIJ/Models/GenerateRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace PetNameGeneratorMIJ.Models
+{
+    public class GenerateRequest
+    {
+        public string AnimalType { get; set; }
+        public bool TwoPart { get; set; }
+    }
+}
